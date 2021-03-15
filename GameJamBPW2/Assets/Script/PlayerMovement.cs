984996a67654fb2390373457e7f815af1c7baf1e
Andries <61500390+Emerald001@users.noreply.github.com>
2021-03-15 15:15:31 +0100

    Car better

--- GameJamBPW2/Assets/Script/PlayerMovement.cs
+++ GameJamBPW2/Assets/Script/PlayerMovement.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float speed;
+    public float speed;
     public float maxSpeed;
+    public float maxBackwardsSpeed;
+    public float rotSpeed;
     public CharacterController player;
 
-    // Update is called once per frame
     void Update()
     {
         float H = Input.GetAxisRaw("Horizontal");
 
         Quaternion newRotation = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w); ;
-        newRotation *= Quaternion.Euler(0, H * 10, 0); // this add a 90 degrees Y rotation
+        newRotation *= Quaternion.Euler(0, H * rotSpeed, 0);
         transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, 4 * Time.deltaTime);
 
         if (Input.GetKey(KeyCode.W)) {
@@ -22,6 +23,13 @@ public class PlayerMovement : MonoBehaviour
         }
         else {
             DownSpeed();
+        }
+
+        if (Input.GetKey(KeyCode.S)) {
+            BackUpSpeed();
+        }
+        else {
+            BackDownSpeed();
         }
 
         transform.position += transform.forward * speed * Time.deltaTime;
@@ -35,6 +43,17 @@ public class PlayerMovement : MonoBehaviour
     void DownSpeed() {
         if (speed > 0) {
             speed -= .5f;
+        }
+    }
+
+    void BackUpSpeed() {
+        if (speed > -maxBackwardsSpeed) {
+            speed -= .5f;
+        }
+    }
+    void BackDownSpeed() {
+        if (speed < 0) {
+            speed += .5f;
         }
     }
 }
