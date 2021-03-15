e62618b113918b7b0c1603666e26b74cb04418d0
Andries <61500390+Emerald001@users.noreply.github.com>
2021-03-15 16:56:41 +0100

    Yeet

--- GameJamBPW2/Assets/Script/Finish.cs
+++ GameJamBPW2/Assets/Script/Finish.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
     public float round;
 
     private void OnTriggerEnter(Collider other) {
-        round--;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
     private void Update() {
-        if(round < 1) {
-            
-        }
+
     }
 }
